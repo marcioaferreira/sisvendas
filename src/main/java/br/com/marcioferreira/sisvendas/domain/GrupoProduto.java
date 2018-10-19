045b507cab23a8cf28f4436ef39ac9f861113984
Marcio Ferreira <marcioaferreira77@gmail.com>
2018-10-19 14:25:09 -0300

    Banco de Dados H2 e geração Base de Dados

--- src/main/java/br/com/marcioferreira/sisvendas/domain/GrupoProduto.java
+++ src/main/java/br/com/marcioferreira/sisvendas/domain/GrupoProduto.java
@@ -2,10 +2,27 @@ package br.com.marcioferreira.sisvendas.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.Columns;
+
+@Entity
 public class GrupoProduto implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
+	@NotNull
+	@NotEmpty
+	@Column()
 	private String descricao;
 
 	public GrupoProduto() {
