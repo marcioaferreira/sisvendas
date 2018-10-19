da657662c4fa72a6540fe2faf30ba984235cecd5
Marcio Ferreira <marcioaferreira77@gmail.com>
2018-10-19 10:08:20 -0300

    Projeto criado

---
+++ src/main/java/br/com/marcioferreira/sisvendas/SisvendasApplication.java
@@ -0,0 +1,12 @@
+package br.com.marcioferreira.sisvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisvendasApplication.class, args);
+	}
+}
