e8673f9f085b9515fd64fd2c07c4d6ec5554c469
Marcio Ferreira <marcioaferreira77@gmail.com>
2018-10-19 11:02:36 -0300

    Testando classe GrupoProduto

--- src/main/java/br/com/marcioferreira/sisvendas/resources/GrupoProdutoResource.java
+++ src/main/java/br/com/marcioferreira/sisvendas/resources/GrupoProdutoResource.java
@@ -1,17 +1,31 @@
 package br.com.marcioferreira.sisvendas.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.marcioferreira.sisvendas.domain.GrupoProduto;
 
 @RestController
 @RequestMapping(value="/gruposprodutos")
 public class GrupoProdutoResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
+	public List<GrupoProduto> listar() {
 		
-		return "REST está funcionando";
+		GrupoProduto p1 = new GrupoProduto(1,"SSD");
+		GrupoProduto p2 = new GrupoProduto(2,"Still");
+		GrupoProduto p3 = new GrupoProduto(3,"Cerveja");
+		
+		List<GrupoProduto> lista = new ArrayList<>();
+		lista.add(p1);
+		lista.add(p2);
+		lista.add(p3);
+		
+		return lista;
 	}
 
 }
